8b67eba444aa4432e6319b1a5df0dfd7ed06069b
MTLChrisLEE <sanghyukchrislee@gmail.com>
2018-06-28 01:45:55 -0400

    Created 3 classes: Tank, Player/AI Controller

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+
+
+
+
+ATank * ATankPlayerController::GetControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Error, TEXT("PLAYER NOT POSSESSING A TANK"))
+	}
+	UE_LOG(LogTemp, Warning, TEXT("PLAYER POSSESSING A TANK %s"), *(ControlledTank->GetName()))
+
+}
