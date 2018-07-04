798262536cbd110e114e1edbae63b32a04c3ada1
MTLChrisLEE <sanghyukchrislee@gmail.com>
2018-07-03 22:17:42 -0400

    Created TankTrack class to move the tank
    Used ApplyForceAtLocation() to apply the force on the track.
    //TODO: Use friction to move the tank on the ground

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankTrack.h"
+
+
+
+
+void UTankTrack::SetThrottle(float Throttle) {
+	
+	UE_LOG(LogTemp, Error, TEXT("%s Throttle %f"), *GetName(), Throttle);
+
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
+
+}
